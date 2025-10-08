2541a8c21c9aee6eece85e16ab62d392726caa2f
Akshay-Jain-sportskeeda <akshay.jain@sportskeeda.com>
2025-10-08 19:16:21 +0530

    Updated page.tsx

--- src/app/draw-memory/page.tsx
+++ src/app/draw-memory/page.tsx
@@ -216,7 +216,7 @@ export default function DrawMemoryPage() {
                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-2.5L13.732 4c-.77-.833-1.964-.833-2.732 0L3.732 16.5c-.77.833.192 2.5 1.732 2.5z" />
                     </svg>
-                    <span className="text-sm font-medium">Hmm, needs more details to recognize it!</span>
+                    <span className="text-sm font-medium">You can do better than that!</span>
                   </div>
                   {/* Arrow pointing down to button */}
                   <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-orange-500"></div>
